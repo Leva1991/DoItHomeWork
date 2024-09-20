031cc4d0b9f85fdf87161fcb8f33aaf73d788c2c
Levani Kutchava <l.kuchava.doit@itstep.ge>
2024-09-21 01:42:56 +0400

    HomeWork2

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,86 @@
+﻿class HomeWork2
+{
+    static void Main()
+    {
+
+        // დავალება 1
+         Console.WriteLine("Enter a Number");
+         int inputNumber = int.Parse(Console.ReadLine());
+         int count = 0;
+        for (int i = 1; i <= inputNumber; i++)
+         {
+             count += i;            
+         }
+         Console.WriteLine(count);
+
+         // დავალება 2
+         Console.WriteLine("Enter a Number");
+         int inputNumber2 = int.Parse(Console.ReadLine());
+         for (int i = 1;i <= 10; i++)
+         {
+             Console.WriteLine($"{inputNumber2} * {i} = {inputNumber2*i}");
+         } 
+
+        // დავალება 3
+        Console.WriteLine("Enter a Number");
+        int inputNumber3 = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= inputNumber3; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        } 
+
+        // დავალება 4
+        Console.WriteLine("Enter a Number");
+        int inputNumber4 = int.Parse(Console.ReadLine());
+        int count2 = 1;
+        for (int i = 1; i <= inputNumber4; i++)
+        {
+            count2 *= i;
+        }
+        Console.WriteLine(count2);  
+
+        // დავალება 5
+        Console.WriteLine("Enter a Number");
+        int inputNumber5 = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= inputNumber5; i++)
+        {
+            for (int j = 1; j <=i; j++)
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine();
+        }
+
+        // დავალება 6
+
+        Console.WriteLine("Enter First Number");
+        int firstNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Second Number");
+        int SecondNumber = int.Parse(Console.ReadLine());
+
+        if (firstNumber > SecondNumber)
+        {
+            for (int i = SecondNumber; i <= firstNumber; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+        else
+        {
+            for (int i = firstNumber; i <= SecondNumber; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
---
+++ lecture3/Program.cs
@@ -0,0 +1,57 @@
+﻿class lecture3
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number");
+        double number1 = double.Parse(Console.ReadLine());
+
+
+        while (true)
+        {
+            try
+            {
+
+                
+                Console.WriteLine("Enter another number");
+                double number2 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter operator");
+                string imputOperator = Console.ReadLine();
+
+
+                if (imputOperator == "/")
+                {
+                    number1 =number1/ number2;
+                    Console.WriteLine(number1);
+                }
+                else if (imputOperator == "-")
+                {
+                    number1 -= number2;
+                    Console.WriteLine(number1);
+                }
+                else if (imputOperator == "+")
+                {
+                    number1 += number2;
+                    Console.WriteLine(number1);
+
+                }
+                else if (imputOperator == "*")
+                {
+                    number1 *= number2;
+                    Console.WriteLine(number1);
+
+                }
+                else if (imputOperator == "stop")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("use correct operator");
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
