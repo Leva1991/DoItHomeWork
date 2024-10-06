32ed51043ae0a19002b25b3b8ba278f8af8e0af4
Levani Kutchava <l.kuchava.doit@itstep.ge>
2024-10-06 14:28:02 +0400

    homework7 part1

---
+++ HomeWork7/Program.cs
@@ -0,0 +1,109 @@
+﻿namespace HomeWork7
+{
+    public class Lkuchava
+    {
+        string name;
+        string lastname;
+        int age;
+        string personalNumber;
+        string phoneNumber;
+        string email;
+
+        public Lkuchava(string name, string lastname, int age, string personalNumber, string phoneNumber, string email)
+        {
+            this.name = name;
+            this.lastname = lastname;
+            this.age = age;
+            this.personalNumber = personalNumber;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+
+        public void printLkuchava()
+        {
+            Console.WriteLine($"{name},{lastname},{age},{personalNumber},{phoneNumber},{email}");
+        }
+    }
+
+    public class Account
+    {
+        private string accountNumber;
+        public string currency;
+        public int balance;
+
+        public string AccountNumber
+        {
+            get
+            {
+                return accountNumber;
+            }
+            set
+            {
+                if (accountNumber.Length == 22)
+                {
+                    accountNumber = value;
+                }
+                else
+                {
+                    Console.WriteLine("use correct account Number");
+                }
+            }
+        }
+        public string Currency
+        {
+            get
+            {
+                return currency;
+            }
+            set
+            {
+                if (currency.Length == 3)
+                {
+                    currency = value;
+                }
+                else
+                {
+                    Console.WriteLine("use correct currency ");
+                }
+            }
+        }
+        public int Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                if (balance > 0)
+                {
+                    balance = value;
+                }
+                else
+                {
+
+                    Console.WriteLine("use correct balance");
+                }
+
+            }
+        }
+    }
+
+    public class Client
+    {
+        public string name;
+        public string lastname;
+        public string personalNumber;
+        Account account;
+    }
+        internal class Program
+        {
+
+            static void Main(string[] args)
+            {
+                Lkuchava lkuchava = new Lkuchava("Levani", "Kuchava", 33, "12345678910", "598264226", "lev.kuchava@gmail.com");
+                lkuchava.printLkuchava();
+            }
+        }
+    }
+
---
+++ lecture6/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace lecture6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArray = { 1, 2, 3, 5, 124, 5, 61, 7, 8 };
+            //sortArrayAsc(testArray);
+            //Console.WriteLine(findElement(testArray, 5555));
+            //Console.WriteLine(findLastIndex(testArray,1));
+           
+        }
+
+
+        static void sortArrayAsc(int[] inputArray)
+        {
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = 0; j < inputArray.Length - i - 1; j++)
+                {
+                    if (inputArray[j] > inputArray[j + 1])
+                    {
+                        int temp = inputArray[j];
+                        inputArray[j] = inputArray[j + 1];
+                        inputArray[j + 1] = temp;
+
+                    }
+                }
+            }
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                Console.WriteLine(inputArray[i]);
+            }
+        }
+
+        static int findElement(int[] inputArray, int element) 
+        {
+            
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+              if(inputArray[i]==element)
+                {
+                  return element;
+                }
+            }
+             return default;
+        }
+
+        static int findLastIndex(int[] inputArray, int element) 
+        {
+           
+            for (int i = inputArray.Length -1; i >= 0; i--)
+            {
+
+                if (inputArray[i] == element)
+                {
+                    return i;
+                }
+               
+            }
+
+            return -1;             
+                 
+           }
+
+        static int[] reverseArray(int[] inputArray) 
+        {
+            int[] tempArray= new int[inputArray.Length];
+            for (int i = 0; i < inputArray.Length; i++) 
+            {
+                tempArray[i]= inputArray[inputArray.Length-1-i];
+            }
+            inputArray= tempArray;
+
+            return tempArray;
+            
+        }
+
+        static int[] findAllElement(int[] inputArray, int element) 
+        {
+            int count = 0;
+            
+            for (int i = 0; i < inputArray.Length; i++) 
+            {
+                if(inputArray[i]==element) {
+                    count++;
+                }
+            }
+
+            int[] tempArray = new int[count];
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                
+                if (inputArray[i] == element)
+                {
+                    count++;
+                }
+            }
+
+
+            return newArray;
+        }
+            
+        
+    }
+}
